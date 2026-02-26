526af89ba4b04c699875fa4cc075d84d8ee26ab2
Salumelian1 <salustianomeliann@gmail.com>
2026-02-26 17:19:32 -0300

    HomeController modificado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,12 +11,32 @@ public class HomeController : Controller
         _movimientoRepository = movimientoRepositorio;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(DateOnly? desde, DateOnly? hasta, string filtro = "mes")
     {
         try
         {
-            var resumen = _movimientoRepository.GetResumen();
-            resumen.Movimientos = _movimientoRepository.GetAll();
+            var hoy = DateOnly.FromDateTime(DateTime.Today);
+            if(desde == null || hasta == null)
+            {
+                switch (filtro)
+                {
+                    case "hoy":
+                        desde = hoy;
+                        hasta = hoy;
+                        break;
+                    case "semana":
+                        desde = hoy.AddDays(-7);
+                        hasta = hoy;
+                        break;
+                    case "mes":
+                    default:
+                        desde = new DateOnly(hoy.Year,hoy.Month,1);
+                        hasta = hoy;
+                        break;
+                }
+            }
+            var resumen = _movimientoRepository.GetResumen(desde.Value,hasta.Value);
+            resumen.Movimientos = _movimientoRepository.GetAll(desde.Value,hasta.Value);
             return View(resumen);
         }
         catch
@@ -25,15 +45,27 @@ public class HomeController : Controller
         }
     }
 
-    public JsonResult GetGastosPorCategoria()
+    public JsonResult GetGastosPorCategoria(DateOnly? desde, DateOnly? hasta)
     {
-        try { return Json(_movimientoRepository.GetGastosPorCategoria()); }
+        try
+        { 
+            var hoy = DateOnly.FromDateTime(DateTime.Today);
+            desde ??= new DateOnly(hoy.Year,hoy.Month,1);
+            hasta??= hoy;
+            return Json(_movimientoRepository.GetGastosPorCategoria(desde.Value,hasta.Value)); 
+        }
         catch { return Json(new List<object>()); }
     }
 
-    public JsonResult GetTopCategorias()
+    public JsonResult GetTopCategorias(DateOnly? desde, DateOnly? hasta)
     {
-        try { return Json(_movimientoRepository.GetTopCategorias()); }
+        try
+        {
+            var hoy = DateOnly.FromDateTime(DateTime.Today);
+            desde ??= new DateOnly(hoy.Year,hoy.Month,1);
+            hasta??= hoy;
+             return Json(_movimientoRepository.GetTopCategorias(desde.Value,hasta.Value)); 
+        }
         catch { return Json(new List<object>()); }
     }
 
